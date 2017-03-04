f27f0b4573732341ceb822eaac7fd5dcb578c078
“daimengxuan” <dai.mengxuan@foxmail.com>
2017-03-04 17:30:20 +0800

    初始化项目

---
+++ first/first/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  first
+//
+//  Created by 戴孟旋 on 2017/3/4.
+//  Copyright © 2017年 戴孟旋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ first/first/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  first
+//
+//  Created by 戴孟旋 on 2017/3/4.
+//  Copyright © 2017年 戴孟旋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
